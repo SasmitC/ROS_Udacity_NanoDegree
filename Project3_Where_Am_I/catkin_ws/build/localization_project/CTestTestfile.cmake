78970a018136ff0e5ba55571533c243978a9e070
Sasmit <sc-171868@hs-weingarten.de>
2019-09-09 14:52:00 +0200

    clear cache

--- Project3_Where_Am_I/catkin_ws/build/localization_project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/catkin_ws/src/localization_project
-# Build directory: /home/workspace/catkin_ws/build/localization_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Project5_Home_Service_Robot/catkin_ws/src/scripts/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xterm -e "source /opt/ros/kinetic/setup.bash; roscore" & 
-sleep 5
-xterm  -e  "gazebo" &
-sleep 5
-xterm  -e  "rosrun rviz rviz"
